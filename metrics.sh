7d09c38c9e6d9e4ffccb3b5681affe81770196e4
jano <jano@deepsix>
2011-01-11 10:14:07 +0100

    Finished migration from Nerus project

--- metrics.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gnatmetric -files metrics.txt -cargs -I. -I../agpl -I../player -I../agpl/xmlada -I../agpl/trace_true -I../agpl/agpl-gdk -I/usr/local/include/gtkada -Iexpres-gtk/visor/src -Iexpres-gtk -Imain -Iplayer -Imbicp -I../agpl/concorde -I../agpl/mw -Iexpres-gtk/main
